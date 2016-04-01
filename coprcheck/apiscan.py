75eda09a410a446a64c0f9f70873afd42e8ebaab
Jan Staněk <jstanek@redhat.com>
2016-04-01 14:52:22 +0200

    Revorked tests structure
    - Added builds interface

--- coprcheck/apiscan.py
+++ coprcheck/apiscan.py
@@ -12,7 +12,7 @@ import requests
 
 COPR_ROOT = 'https://copr.fedorainfracloud.org'
 MONITOR_URL = '/api/coprs/{user}/{project}/monitor'
-BUILD_TASK_URL = '/api_2/build_tasks'
+BUILD_URL = '/api_2/build/{build_id:d}'
 
 
 # Results wrapper
@@ -24,7 +24,7 @@ BuildResult.__doc__ += ': Container for COPR build result info.'
 #BuildResult.url.__doc__ = 'Absolute URL of the resulting artifacts.'
 
 # Possible API contact errors
-ConnectionError = requests.ConnectionError
+ConnectionError = requests.exceptions.ConnectionError
 
 HTTPError = requests.HTTPError
 
@@ -53,12 +53,13 @@ def monitor(user: str, project: str) -> dict:
 
     rsp = requests.get(''.join([COPR_ROOT, MONITOR_URL.format(
             user=user, project=project)]))
-    data = rsp.json()
 
-    if rsp.status_code == 200:
-        return data
+    if rsp.status_code == requests.codes.ok:
+        return rsp.json()
+    elif rsp.status_code == requests.codes.not_found:
+        raise ProjectNotFoundError(rsp.json()['error'])
     else:
-        raise ProjectNotFoundError(data['error'])
+        rsp.raise_for_status()
 
 
 def build(build_id: int) -> dict:
@@ -77,7 +78,16 @@ def build(build_id: int) -> dict:
         HTTPError -- On general server errors.
     """
 
-    raise NotImplementedError()
+    rsp = requests.get(
+            ''.join([COPR_ROOT, BUILD_URL.format(build_id=build_id)]),
+            params={'show_build_tasks': True})
+
+    if rsp.status_code == requests.codes.ok:
+        return rsp.json()
+    elif rsp.status_code == requests.codes.not_found:
+        raise BuildNotFoundError(rsp.json()['message'])
+    else:
+        rsp.raise_for_status()
 
 
 def current_builds(user: str, project: str): # Generator[BuildResult, None, None]
--- tests/unit/test_apiscan.py
+++ tests/unit/test_apiscan.py
@@ -1,73 +1,123 @@
 """Unit tests for the apiscan module of the coprcheck package."""
 
-import pytest
+from collections import namedtuple
 import re
+
+import pytest
 import requests
 import responses
 
 from coprcheck import apiscan as ascn
 
 
+# ### Tools for parametrization ###
+MockData = namedtuple('MockData', ['scheme', 'valid', 'invalid'])
+"""Structure of mock test data/parametres."""
+
+ParameterDecorators = namedtuple('ParameterDecorators', ['all', 'valid', 'invalid'])
+"""Set of decorators for passing parameters to tests."""
+
+def make_param_decorators(data: MockData) -> ParameterDecorators:
+    """Create decorators from test data."""
+    return ParameterDecorators(
+            all=pytest.mark.parametrize(data.scheme, data.valid+data.invalid),
+            valid=pytest.mark.parametrize(data.scheme, data.valid),
+            invalid=pytest.mark.parametrize(data.scheme, data.invalid))
+
 # ### Common test data ###
-PROJECT_SCHEMA = 'user,project'
-VALID_PROJECTS = [('jstanek','udiskie'),]
-INVALID_PROJECTS = [('nobody', 'nothing'),]
+MOCK_PROJECTS = MockData(
+        scheme='user,project',
+        valid=[('jstanek', 'udiskie')],
+        invalid=[('nobody', 'nothing')])
+for_projects = make_param_decorators(MOCK_PROJECTS)
 
-for_all_projects = pytest.mark.parametrize(
-        PROJECT_SCHEMA, VALID_PROJECTS+INVALID_PROJECTS)
-
-for_valid_projects = pytest.mark.parametrize(
-        PROJECT_SCHEMA, VALID_PROJECTS)
-
-for_invalid_projects = pytest.mark.parametrize(
-        PROJECT_SCHEMA, INVALID_PROJECTS)
+MOCK_BUILDS = MockData(
+        scheme='build_id',
+        valid=[42],
+        invalid=[99])
+for_builds = make_param_decorators(MOCK_BUILDS)
 
 # ### Fixtures ###
 @pytest.fixture
-def mock_requests():
-    """Mocked requests library."""
-    return responses.RequestsMock()
-
-@pytest.fixture
-def mock_no_connectivity(mock_requests):
+def mock_no_connectivity():
     """Mocked requests to always raise ConnectionError."""
+
+    mock_requests = responses.RequestsMock()
     mock_requests.add(responses.GET, re.compile('.*'),
                       body=requests.ConnectionError('No connectivity!'))
     return mock_requests
 
-@pytest.fixture
-def mock_monitor(mock_requests):
-    """Mocked response from COPR monitor."""
-    url = lambda u, p: ''.join([
-            ascn.COPR_ROOT, ascn.MONITOR_URL.format(user=u, project=p)])
-
-    for user, project in VALID_PROJECTS:
-        mock_requests.add(
-                responses.GET, url(user, project),
-                status=200, json={'status': 'succeeded'})
-
-    for user, project in INVALID_PROJECTS:
-        mock_requests.add(
-                responses.GET, url(user, project),
-                status=404,
-                json={'status': 'failed', 'error': 'NotFound'})
-
-    mock_requests.assert_all_requests_are_fired = False
-    return mock_requests
-
 # ### Monitor tests ###
 
-@for_valid_projects
-def test_monitor_success(mock_monitor, user, project):
-    with mock_monitor:
-        assert ascn.monitor(user, project) == {'status': 'succeeded'}
+MONITOR_URL = ascn.COPR_ROOT + ascn.MONITOR_URL
 
-@for_all_projects
+@for_projects.valid
+@responses.activate
+def test_monitor_success(user, project):
+    responses.add(responses.GET, url=MONITOR_URL.format(user=user, project=project),
+                  status=200, json={'builds': []})
+
+    monitor_data = ascn.monitor(user, project)
+    assert 'builds' in monitor_data
+
+@for_projects.all
 def test_monitor_no_connectivity(mock_no_connectivity, user, project):
     with mock_no_connectivity, pytest.raises(ascn.ConnectionError):
-        ascn.monitor(user, project)
+        monitor_data = ascn.monitor(user, project)
 
-@for_invalid_projects
-def test_monitor_no_project(mock_monitor, user, project):
-    with mock_monitor, pytest.raises(ascn.ProjectNotFoundError):
-        ascn.monitor(user, project)
+@for_projects.invalid
+@responses.activate
+def test_monitor_no_project(user, project):
+    responses.add(responses.GET, url=MONITOR_URL.format(user=user, project=project),
+                  status=404, json={'error': 'Project does not exists.'})
+
+    with pytest.raises(ascn.ProjectNotFoundError):
+        monitor_data = ascn.monitor(user, project)
+
+@for_projects.all
+@responses.activate
+def test_monitor_server_error(user, project):
+    responses.add(responses.GET, url=MONITOR_URL.format(user=user, project=project),
+                  status=500, content_type='test/plain',
+                  body='500 Internal Server Error')
+
+    with pytest.raises(ascn.HTTPError):
+        monitor_data = ascn.monitor(user, project)
+
+# ### Build tests ###
+
+BUILD_URL = ascn.COPR_ROOT + ascn.BUILD_URL
+
+@for_builds.valid
+@responses.activate
+def test_build_success(build_id):
+    responses.add(responses.GET, url=BUILD_URL.format(build_id=build_id),
+                  status=200, json={'build': {}})
+
+    build_data = ascn.build(build_id)
+    print(responses.calls[0])
+    assert 'build' in build_data
+
+@for_builds.all
+def test_build_no_connectivity(mock_no_connectivity, build_id):
+    with mock_no_connectivity, pytest.raises(ascn.ConnectionError):
+        build_data = ascn.build(build_id)
+
+@for_builds.invalid
+@responses.activate
+def test_build_nonexistent(build_id):
+    responses.add(responses.GET, url=BUILD_URL.format(build_id=build_id),
+                  status=404, json={'message': 'Build not found.'})
+
+    with pytest.raises(ascn.BuildNotFoundError):
+        build_data = ascn.build(build_id)
+
+@for_builds.all
+@responses.activate
+def test_build_server_error(build_id):
+    responses.add(responses.GET, url=BUILD_URL.format(build_id=build_id),
+                  status=500, content_type='text/plain',
+                  body='500 Internal Server Error')
+
+    with pytest.raises(ascn.HTTPError):
+        build_data = ascn.build(build_id)
